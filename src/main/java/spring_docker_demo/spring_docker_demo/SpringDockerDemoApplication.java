7fa633b9aef87afe069c878648e638254f9f61bd
piyali1213 <piyalidas173@gmail.com>
2024-10-25 08:21:56 -0400

    first commit

---
+++ src/main/java/spring_docker_demo/spring_docker_demo/SpringDockerDemoApplication.java
@@ -0,0 +1,21 @@
+package spring_docker_demo.spring_docker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerDemoApplication {
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "welcome to spring docker demo";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_docker_demo/spring_docker_demo/SpringDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_docker_demo.spring_docker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
